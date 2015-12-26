0fa8c5bf29a4fc6ef471e23db6c6b59d040feaa0
Alexandr <intel777chanel@gmail.com>
2015-12-26 13:04:51 +0200

    Main script file

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests as r, random, os
+
+allpic=''
+downloads=0
+
+if os.path.exists('wallhaven'):
+    print('Folder alredy exists')
+else:
+    os.makedirs('wallhaven')
+    print('Working folder successfuly created')
+os.chdir('wallhaven')
+allpic=input('Enter current amount of pictures: ')
+while downloads < allpic:
+    ext='jpg'
+    picid=random.randint(1,allpic)
+    pic=r.get('http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{}.{}'.format(picid,ext))
+    if pic == '<Response [404]>':
+        ext='png'
+        pic=r.get('http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{}.{}'.format(picid,ext))
+    picname='wallhaven-{}.{}'.format(picid,ext)
+    if os.path.exists(picname):
+        print('Image with ID:{} alredy exists'.format(picid))
+    else:
+        out=open('{}'.format(picname), 'wb')
+        out.write(pic.content)
+        out.close()
+    if os.path.getsize(picname) == 162:
+        print('Image with id:{} NOT FOUND(404 response). Deleting file...'.format(picid))
+        os.remove(picname)
+    else:
+        print('Image {}/{} downloaded. ID:{}'.format(downloads,allpic,picid))
+    downloads = downloads + 1
+print('Done!')
